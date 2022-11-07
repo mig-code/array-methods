d2d34ea8c54accbcd9b636c683a04940c8a121ee
Miguel <99726342+mig-code@users.noreply.github.com>
2022-11-07 14:05:43 +0100

    Minor change in test

--- modules/sample.test.js
+++ modules/sample.test.js
@@ -1,11 +1,11 @@
 import { sum } from './sample.js';
 
 describe('Given sum function', () => {
-    const value1 = 2;
+    const value1 = 3;
     const value2 = 5;
-    describe('When arguments are 2 and 5', () => {
-        test('Then result should be 7', () => {
-            const expected = 7;
+    describe('When arguments are 3 and 5', () => {
+        test('Then result should be 8', () => {
+            const expected = 8;
             const r = sum(value1, value2);
             expect(r).toBe(expected);
         });
