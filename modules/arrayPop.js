b8464a9549be1c5ec906060eaeba054140b3c59c
Miguel <99726342+mig-code@users.noreply.github.com>
2022-11-08 15:34:02 +0100

    Add test for return

--- modules/arrayPop.js
+++ modules/arrayPop.js
@@ -2,5 +2,7 @@ import { arrayLength } from './arrayLength.js';
 
 export function arrayPop(arr) {
     const length = arrayLength(arr) ? arrayLength(arr) - 1 : 0;
+    const lastElement = arrayLength(arr) ? arr[length] : undefined;
     Object.defineProperty(arr, 'length', { value: length });
+    return lastElement;
 }
--- modules/arrayPop.test.js
+++ modules/arrayPop.test.js
@@ -3,25 +3,29 @@ const cases = [
     {
         arr: [1, 2, 3],
         expected: [1, 2],
+        lastElement: 3,
     },
 
     {
         arr: [],
         expected: [],
+        lastElement: undefined,
     },
     {
         arr: [1, 2, ['a', 'b']],
         expected: [1, 2],
+        lastElement: ['a', 'b'],
     },
 ];
 
 describe('Given arrayPop function', () => {
     cases.forEach((item) => {
-        test(`Then result of pop ${item.arr} should be ${item.expected}`, () => {
+        test(`Then result of pop ${item.arr} should be ${item.expected} and return last element ${item.lastElement}`, () => {
             const expected = item.expected;
             const r = item.arr;
-            arrayPop(item.arr);
+            const lastElement = arrayPop(item.arr);
             expect(r).toEqual(expected);
+            expect(lastElement).toEqual(item.lastElement);
         });
     });
 });
