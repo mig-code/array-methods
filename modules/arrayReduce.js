609f053d5172f6de604aec68bb11ccd1b6f6d183
Miguel <99726342+mig-code@users.noreply.github.com>
2022-11-10 14:22:21 +0100

    Add new version reduce function

--- modules/arrayReduce.js
+++ modules/arrayReduce.js
@@ -1,17 +1,19 @@
 import { arrayLength } from './arrayLength.js';
 export function arrayReduce(arr, callback, initialValue) {
-    let accumulate = initialValue;
+    let accumulate = arrayLength(arguments) > 2 ? initialValue : arr[0];
 
-    if (arrayLength(arr) === 0) throw new Error('Empty array');
+    if (arrayLength(arr) === 0 && arrayLength(arguments) === 2)
+        throw new Error('Empty array');
 
-    // NOT WORKING
     if (arrayLength(arr) === 1) {
-        return initialValue === undefined ? NaN : initialValue;
+        arrayLength(arguments) === 2
+            ? (accumulate = arr[0])
+            : (accumulate = callback(initialValue, arr[0]));
+        return accumulate;
     }
 
-    for (let i = 0; i < arrayLength(arr); i++) {
+    for (let i = 1; i < arrayLength(arr); i++) {
         accumulate = callback(accumulate, arr[i]);
     }
     return accumulate;
 }
-// NEEDS TO BE FIXED FOR ARRAY OF 1 ELEMENT
--- modules/arrayReduce.test.js
+++ modules/arrayReduce.test.js
@@ -1,29 +1,47 @@
 import { arrayReduce } from './arrayReduce';
-const cases = [
+
+// I have to use two cases because if i use always initial value the function will take three arguments and will not work
+const casesWithoutInitial = [
     {
         arr: [1, 2, 3, 4],
         function: (acc, item) => acc * item,
-        initialValue: 0,
         expected: 0,
     },
     {
         arr: [0, 1, 2, 3, 4],
         function: (acc, item) => acc * item,
-        initialValue: 1,
         expected: 0,
     },
-    // THIS EXAMPLE NEEDS TO BE FIXED
-    // {
-    //     arr: [1],
-    //     function: (acc, item) => acc * item,
-    //     initialValue: undefined,
-    //     expected: 1,
-    // },
+
+    {
+        arr: [1],
+        function: (acc, item) => acc * item,
+        expected: 1,
+    },
     {
         arr: ['a', 'b', 'c', 'd'],
         function: (acc, item) => acc + item,
-        initialValue: undefined,
         expected: 'abcd',
+    },
+];
+const casesWithInitial = [
+    {
+        arr: [1, 2, 3, 4],
+        function: (acc, item) => acc * item,
+        initialValue: 1,
+        expected: 24,
+    },
+    {
+        arr: ['a', 'b', 'c', 'd'],
+        function: (acc, item) => acc + item,
+        initialValue: 'b',
+        expected: 'bbcd',
+    },
+    {
+        arr: [1],
+        function: (acc, item) => acc * item,
+        initialValue: 4,
+        expected: 4,
     },
 ];
 const errorCases = [
@@ -35,10 +53,17 @@ const errorCases = [
 ];
 
 describe('Given arrayJoin function', () => {
-    cases.forEach((item) => {
-        test(`Then result of Reduce ${item.arr} with ${item.function} should be ${item.expected}`, () => {
-            const expected = item.arr.reduce(item.function, item.initialValue);
-            // const expected = item.expected;
+    casesWithoutInitial.forEach((item) => {
+        test(`Then result of Reduce  ${item.arr} WITHOUT INITIAL VALUE with ${item.function} should be ${item.expected}`, () => {
+            const expected = item.arr.reduce(item.function);
+            const r = arrayReduce(item.arr, item.function);
+            expect(r).toEqual(expected);
+        });
+    });
+    casesWithInitial.forEach((item) => {
+        test(`Then result of Reduce ${item.arr} WITH INITIAL VALUE with ${item.function} should be ${item.expected}`, () => {
+            // I tested with 2 different ways to get the expected result
+            const expected = item.expected;
             const r = arrayReduce(item.arr, item.function, item.initialValue);
             expect(r).toEqual(expected);
         });
